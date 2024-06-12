8a6898f8136cbbce3739c73f99253f66087380a7
Kimyoungwoo11 <kyw090966@naver.com>
2024-06-12 16:28:33 +0900

    게시판작업중1

---
+++ src/main/java/com/example/project/dto/BoardDTO.java
@@ -0,0 +1,24 @@
+package com.example.project.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Alias("templates/board")
+public class BoardDTO {
+    private String board_no;
+    private String id;
+    private String name;
+    private String title;
+    private String content;
+    private Date write_date;
+    private String category;
+
+}
