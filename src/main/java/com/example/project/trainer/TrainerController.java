651b14c662ea6341108dceac6fad7ed88ec22330
dptjd324 <dptjd324@naver.com>
2024-06-12 18:53:36 +0900

    GYM TRAINER

---
+++ src/main/java/com/example/project/trainer/TrainerController.java
@@ -0,0 +1,12 @@
+package com.example.project.trainer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TrainerController {
+    @GetMapping("/trainer")
+    public String trainerpage(){
+        return "trainer/trainerhome";
+    }
+}
