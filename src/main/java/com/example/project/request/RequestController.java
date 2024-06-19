5e6812f38743dd63b1d68ded2af6a403fcbbd4a1
YouYoungSun <ffdr803@naver.com>
2024-06-19 13:49:12 +0900

    [변경]요청컨트롤러,홈컨트롤러

--- src/main/java/com/example/project/request/RequestController.java
+++ src/main/java/com/example/project/request/RequestController.java
@@ -1,7 +1,9 @@
 package com.example.project.request;
 
 
+import com.example.project.login.UserDTO;
 import jakarta.servlet.http.HttpSession;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,7 +18,9 @@ public class RequestController {
 
     @PostMapping("/sendRequest")
     public String sendRequest(@RequestParam("request") String requestContent, HttpSession session) {
-        String userId = (String) session.getAttribute("id");
+        UserDTO member = (UserDTO)session.getAttribute("member");
+        String userId = (String)member.getLoginId();
+        System.out.println(userId);
         RequestDTO requestDTO = new RequestDTO(requestContent);
         requestService.saveRequest(userId, requestDTO);
         System.out.println("요청정보" + requestContent);
@@ -27,6 +31,7 @@ public class RequestController {
     public String viewRequests(Model model) {
         System.out.println("요청뷰" + requestService.getAllRequests());
         model.addAttribute("requests", requestService.getAllRequests());
+        System.out.println(requestService.getAllRequests());
         return "admin/requests";
     }
 
