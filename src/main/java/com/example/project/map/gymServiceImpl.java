b1a8874cbb8fc8bd2ebb2383da3d09172c816c69
junseok01 <wns526@daum.net>
2024-06-22 12:14:32 +0900

    강의 부분 템플릿및 지도수정

--- src/main/java/com/example/project/map/gymServiceImpl.java
+++ src/main/java/com/example/project/map/gymServiceImpl.java
@@ -1,16 +1,23 @@
 package com.example.project.map;
 
 import com.example.project.dto.Gym;
+import com.example.project.dto.GymMapResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class gymServiceImpl implements gymService {
-    private final gymDAO gymDAO;
+    private final gymDAO dao;
     @Override
-    public List<Gym> gymselectlist() {
-        return gymDAO.gymselectlist();
+    public List<GymMapResponseDTO> gymselectlist() {
+        List<Gym> list = dao.gymselectlist();
+        List<GymMapResponseDTO> gymlst = list.stream()
+                .map(GymMapResponseDTO :: new)
+                .collect(Collectors.toList());
+        return gymlst;
     }
 }
