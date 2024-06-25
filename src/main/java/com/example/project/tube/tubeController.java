afb15a7a0a0ac22ed68b6358872672a6cd303cf1
junseok01 <wns526@daum.net>
2024-06-25 17:21:47 +0900

    강의 페이지 검색 및 카테고리 클릭 구현

--- src/main/java/com/example/project/tube/tubeController.java
+++ src/main/java/com/example/project/tube/tubeController.java
@@ -1,12 +1,42 @@
 package com.example.project.tube;
 
+import com.example.project.dto.tubeResponseDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 @Controller
+@RequiredArgsConstructor
 public class tubeController {
+    private final tubeService tubeService;
     @GetMapping("/tube")
-    public String tubepage(){
+    public String tubepage(Model model){
+        List<tubeResponseDTO> tubelist = tubeService.tubelist();
+        model.addAttribute("tubelist",tubelist);
+        return "tube/tubepage2";
+    }
+    @GetMapping("/tubecategory")
+    public String tubeCategory(@RequestParam("category") String category,Model model){
+        List<tubeResponseDTO> tubecategorylist = null;
+        if(category.equals("전체")){
+             tubecategorylist = tubeService.tubelist();
+        }else {
+             tubecategorylist = tubeService.tubecategorylist(category);
+        }
+        model.addAttribute("category",category);
+        model.addAttribute("tubelist",tubecategorylist);
+        return "tube/tubepage2";
+    }
+    @PostMapping("/tubesearch")
+    public String tubeSelect(@RequestParam("keyword") String keyword, Model model){
+        List<tubeResponseDTO> tubeselectlist = tubeService.tubeselectlist(keyword);
+        model.addAttribute("tubelist",tubeselectlist);
+        System.out.println(keyword);
         return "tube/tubepage2";
     }
 }
---
+++ src/main/java/com/example/project/tube/tubeRepository.java
@@ -0,0 +1,24 @@
+package com.example.project.tube;
+
+import com.example.project.dto.tube;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface tubeRepository extends JpaRepository<tube,Long> {
+    List<tube> findByCategory(String category);
+//    @Query(
+//            value = "select * from tube m where m.exercisename=?1 or m.category=?1 or m.tubename=?1",
+//            nativeQuery = true
+//    )
+//    List<tube> findBySearch(String keyword);
+
+    @Query(
+                value = "select * from tube m where m.exercisename like '%' || :keyword || '%' or m.category like '%' || :keyword || '%' or m.tubename like '%' || :keyword || '%'",
+            nativeQuery = true
+    )
+    List<tube> findBySearch(String keyword);
+
+}
+
