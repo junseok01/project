f42e6b969afc4a381b8f9a28867e8d2bcf0cfcbb
junseok01 <wns526@daum.net>
2024-06-26 16:10:41 +0900

    지도 메인페이지 완료

--- src/main/java/com/example/project/map/gymDAO.java
+++ src/main/java/com/example/project/map/gymDAO.java
@@ -5,7 +5,7 @@ import com.example.project.dto.Gym;
 import java.util.List;
 
 public interface gymDAO {
-    public List<Gym> gymlist();
+    public List<Gym> gymlist(float x,float y);
     public List<Gym> gymselectlist(String keyword);
     public List<Gym> gymselectaddrlist(String keyword);
 }
