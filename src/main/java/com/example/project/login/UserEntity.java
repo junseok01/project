06cc59f5fb77542c6aba8f835928b5d54aee3809
YouYoungSun <ffdr803@naver.com>
2024-07-03 15:47:15 +0900

    찜기능 결제기능 구현중

--- src/main/java/com/example/project/login/UserEntity.java
+++ src/main/java/com/example/project/login/UserEntity.java
@@ -1,13 +1,14 @@
 package com.example.project.login;
 
 
+import com.example.project.gym.GymBoardEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.util.Date;
+import java.util.*;
 
 @Entity
 @Table(name = "member")
@@ -32,8 +33,17 @@ public class UserEntity {
     private Date joinDate;
     @Column
     private int loginType;
+    //결재를 위한 포인트
     @Column
     private Integer point=0;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_hearts",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "gymboard_id")
+    )
+    private List<GymBoardEntity> heartedGyms = new ArrayList<>();
 
     public UserEntity(String loginId, String loginPw, String name, String nickname, String cellphoneNo) {
         this.loginId = loginId;
@@ -67,5 +77,22 @@ public class UserEntity {
         this.nickname = nickname;
         this.userType = userType;
         this.loginType = loginType;
+    }
+
+    public void heartGym(GymBoardEntity gymBoard) {
+        // 이미 하트를 누른 게시물인지 확인
+        if (!heartedGyms.contains(gymBoard)) {
+            gymBoard.incrementHeartCount();
+            heartedGyms.add(gymBoard);
+        }
+    }
+
+    public void unheartGym(GymBoardEntity gymBoard) {
+        if (heartedGyms.contains(gymBoard)) {
+            gymBoard.decrementHeartCount();
+            System.out.println(gymBoard.hashCode() + " : " + heartedGyms);
+            heartedGyms.remove(gymBoard);
+            System.out.println("삭제 후 언하트 입니다 +++++++++" + heartedGyms);
+        }
     }
 }
