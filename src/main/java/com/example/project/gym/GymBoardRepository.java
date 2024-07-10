abb1306ccc7092e63cc1c4b97bf8afad4b0026d1
YouYoungSun <ffdr803@naver.com>
2024-07-11 00:54:04 +0900

    메인페이지 변경

--- src/main/java/com/example/project/gym/GymBoardRepository.java
+++ src/main/java/com/example/project/gym/GymBoardRepository.java
@@ -8,10 +8,16 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface GymBoardRepository extends JpaRepository<GymBoardEntity,Long> {
     Page<GymBoardEntity> findByGymnameContaining(String gymname, Pageable pageRequest);
     Page<GymBoardEntity> findByGymaddrContaining(String gymaddr,Pageable pageRequest);
 
     GymBoardEntity findByGymboardnum (Long gymboardnum);
+
+    // heartCount가 많은 순으로 상위 10개 항목을 가져오는 쿼리
+    // 메서드 이름 기반 쿼리 생성 사용
+    List<GymBoardEntity> findTop12ByOrderByHeartCountDesc();
 
 }
